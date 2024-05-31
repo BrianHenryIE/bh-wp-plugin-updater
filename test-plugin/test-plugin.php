2bf4bc59669db17453503e190bec553f857c432f
Brian Henry <BrianHenryIE@gmail.com>
2024-05-30 22:06:31 -0700

    attempt

--- test-plugin/test-plugin.php
+++ test-plugin/test-plugin.php
@@ -10,12 +10,31 @@
  * @package brianhenryie/bh-wp-slswc-client
  */
 
-require_once __DIR__ . '/vendor/autoload.php';
 
-if ( file_exists( __DIR__ . '/src/bh-wp-plugin-meta-kit/bootstrap.php' ) ) {
-	require_once __DIR__ . '/src/bh-wp-plugin-meta-kit/bootstrap.php';
+
+if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	echo 'uhoh' . '<br/>' . PHP_EOL;
+
+	$r = exec( '/usr/local/bin/composer install', $output, $result_code );
+
+	echo 'Composer install result code: ' . $result_code . '<br/>' . PHP_EOL;
+	echo 'Composer install output: ' . '<br/>' . PHP_EOL;
+	echo implode( '<br/>' . PHP_EOL, $output ) . '<br/>' . PHP_EOL;
+
+	echo 'Composer install done' . '<br/>' . PHP_EOL;
 }
 
-if ( file_exists( require_once __DIR__ . '/src/bootstrap.php' ) ) {
-	require_once __DIR__ . '/src/bootstrap.php';
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+} else {
+	echo './vendor/autoload.php not found' . '<br/>' . PHP_EOL;
 }
+
+
+// if ( file_exists( __DIR__ . '/src/bh-wp-plugin-meta-kit/bootstrap.php' ) ) {
+// require_once __DIR__ . '/src/bh-wp-plugin-meta-kit/bootstrap.php';
+// }
+//
+// if ( file_exists( require_once __DIR__ . '/src/bootstrap.php' ) ) {
+// require_once __DIR__ . '/src/bootstrap.php';
+// }
