804e5c694fe2fd4126dbc7a03e484c16d93908cb
Brian Henry <BrianHenryIE@gmail.com>
2024-05-31 12:03:08 -0700

    Update test-plugin.php

--- test-plugin/test-plugin.php
+++ test-plugin/test-plugin.php
@@ -11,30 +11,75 @@
  */
 
 
+namespace BrianHenryIE\WP_SLSWC_Client_Test_Plugin;
 
-if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-	echo 'uhoh' . '<br/>' . PHP_EOL;
+use BrianHenryIE\WP_SLSWC_Client\SLSWC_Client;
 
-	$r = exec( '/usr/local/bin/composer install', $output, $result_code );
+require_once __DIR__ . '/vendor/autoload.php';
 
-	echo 'Composer install result code: ' . $result_code . '<br/>' . PHP_EOL;
-	echo 'Composer install output: ' . '<br/>' . PHP_EOL;
-	echo implode( '<br/>' . PHP_EOL, $output ) . '<br/>' . PHP_EOL;
 
-	echo 'Composer install done' . '<br/>' . PHP_EOL;
+
+add_action(
+	'plugins_loaded',
+	function () {
+		$settings = new \BrianHenryIE\WP_SLSWC_Client\Settings(
+			plugin_basename( __FILE__ ),
+			'https://localhost:8889'
+		);
+
+		SLSWC_Client::get_instance(
+			$settings,
+			new \Psr\Log\NullLogger()
+		);
+	}
+);
+
+/**
+ * @hooked admin_enqueue_scripts
+ */
+function example_admin_enqueue_scripts() {
+	$plugin_slug = explode( '/', plugin_basename( __FILE__ ) )[0];
+
+	$script_handle = "{$plugin_slug}-licence";
+
+	// Only load the JS on the plugin information modal for this plugin.
+	global $pagenow;
+	if ( 'plugin-install.php' !== $pagenow
+		|| ! isset( $_GET['plugin'] )
+		|| sanitize_key( wp_unslash( $_GET['plugin'] ) !== $plugin_slug )
+	) {
+		return;
+	}
+
+	$asset_file = include plugin_dir_path( __FILE__ ) . 'vendor/brianhenryie/bh-wp-slswc-client/build/index.asset.php';
+
+	wp_enqueue_script(
+		$script_handle,
+//		plugins_url( 'vendor/brianhenryie/bh-wp-slswc-client/build/index.js', __FILE__ ), // 404 - correct path
+		// plugins_url( '../build/index.js', __FILE__ ), // 500 - also a correct path
+		 plugins_url( './build/index.js', __FILE__ ),
+		$asset_file['dependencies'],
+		$asset_file['version'],
+		true
+	);
+
+	$api = SLSWC_Client::get_instance();
+
+	$data = wp_json_encode(
+		array(
+			'ajaxUrl'         => admin_url( 'admin-ajax.php' ),
+			'nonce'           => wp_create_nonce( \BrianHenryIE\WP_SLSWC_Client\WP_Includes\AJAX::class ),
+			'licence_details' => $api->get_licence_details(),
+		)
+	);
+
+	// `bh-wc-zelle-gateway-licence` -> `bhWcZelleGatewayLicence`;
+	$script_var_name = lcfirst( str_replace( ' ', '', ucwords( str_replace( '-', ' ', $script_handle ) ) ) );
+
+	wp_add_inline_script(
+		$script_handle,
+		"const {$script_var_name} = {$data};",
+		'before'
+	);
 }
-
-if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-	require_once __DIR__ . '/vendor/autoload.php';
-} else {
-	echo './vendor/autoload.php not found' . '<br/>' . PHP_EOL;
-}
-
-
-// if ( file_exists( __DIR__ . '/src/bh-wp-plugin-meta-kit/bootstrap.php' ) ) {
-// require_once __DIR__ . '/src/bh-wp-plugin-meta-kit/bootstrap.php';
-// }
-//
-// if ( file_exists( require_once __DIR__ . '/src/bootstrap.php' ) ) {
-// require_once __DIR__ . '/src/bootstrap.php';
-// }
+add_action( 'admin_enqueue_scripts', '\BrianHenryIE\WP_SLSWC_Client_Test_Plugin\example_admin_enqueue_scripts' );
