e1e2f351df886bf76040c19156d3fe44f90bd4f4
Brian Henry <BrianHenryIE@gmail.com>
2024-06-25 20:33:20 -0700

    `WordPress_Updater` improvements + test

--- includes/wp-includes/class-wordpress-updater.php
+++ includes/wp-includes/class-wordpress-updater.php
@@ -8,15 +8,20 @@
 namespace BrianHenryIE\WP_SLSWC_Client\WP_Includes;
 
 use BrianHenryIE\WP_SLSWC_Client\API_Interface;
-use BrianHenryIE\WP_SLSWC_Client\Exception\Licence_Key_Not_Set_Exception;
 use BrianHenryIE\WP_SLSWC_Client\Model\Plugin_Update;
 use BrianHenryIE\WP_SLSWC_Client\Settings_Interface;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
 use stdClass;
 
+/**
+ * @phpstan-type Plugin_Update_Array array{}
+ * @phpstan-type Plugin_Data_Array array{}
+ */
 class WordPress_Updater {
 	use LoggerAwareTrait;
+
+	protected bool $force_refresh = false;
 
 	/**
 	 * Constructor.
@@ -34,7 +39,7 @@ class WordPress_Updater {
 	}
 
 	/**
-	 * Add the plugin's update information to the `update_plugins` transient. To be used later on plugins.php.
+	 * Determine should the cached plugin information be used, or should a synchronous request be made.
 	 *
 	 * When a site admin deleted the `update_plugins` transient, i.e. `wp transient delete update_plugins --network`,
 	 * that means they want to force a check for updates. In those cases, i.e. when this plugin does not already have
@@ -43,91 +48,90 @@ class WordPress_Updater {
 	 * Where the plugin is already in the transient, the value will be updated with the saved information, which
 	 * itself is updated with the cron job.
 	 *
-	 * // TODO: maybe split the synchronous/asynchronous requests into a separate method. see "update_plugins_{$hostname}"
-	 *
-	 * @param false|stdClass{last_checked:int, no_update: array<stdClass>, response: array<stdClass>, translations: array} $value
-	 * @param string                                                                                                       $transient Always 'update_plugins'.
-	 *
-	 * @see wp_plugin_update_row()
-	 *
 	 * @hooked pre_set_site_transient_update_plugins
+	 * @see wp_update_plugins()
+	 *
+	 * @param false|stdClass $value
+	 * @param string         $transient Always 'update_plugins'.
+	 *
+	 * @return false|stdClass Always the unchanged input value.
 	 */
-	public function add_product_data_to_wordpress_plugin_information( $value, string $transient ) {
+	public function detect_force_update( $value, string $transient ) {
 
-		// Probably only on a fresh install of WordPress.
+		// Probably only happens on a fresh installation of WordPress.
 		if ( false === $value ) {
 			return $value;
 		}
 
 		// Do a synchronous refresh if the plugin is not already in the `update_plugins` transient.
-		$should_refresh = ! isset( $value->response[ $this->settings->get_plugin_basename() ] )
-			&& ! isset( $value->no_update[ $this->settings->get_plugin_basename() ] );
+		$this->force_refresh = ! isset( $value->response[ $this->settings->get_plugin_basename() ] )
+							&& ! isset( $value->no_update[ $this->settings->get_plugin_basename() ] );
+
+		/**
+		 * The `pre_set_site_transient_update_plugins` filter gets called twice in {@see wp_update_plugins()}. We don't
+		 * need it on the later run.
+		 */
+		remove_filter( 'pre_set_site_transient_update_plugins', array( $this, 'detect_force_update' ) );
+
+		return $value;
+	}
+
+	/**
+	 * Add the plugin's update information to the `update_plugins` transient. To be used later on plugins.php.
+	 *
+	 * @hooked update_plugins_{$hostname}
+	 * @see wp-includes/update.php:513
+	 * @see wp_update_plugins()
+	 *
+	 * @param false|Plugin_Update_Array $value
+	 * @param Plugin_Data_Array         $plugin_data
+	 * @param string                    $plugin_file The plugin basename.
+	 * @param array                     $locales
+	 *
+	 * @return false|Plugin_Update_Array
+	 */
+	public function add_update_information( $afalse, $plugin_data, $plugin_file, $locales ) {
+
+		if ( $this->settings->get_plugin_basename() !== $plugin_file ) {
+			return $afalse;
+		}
 
 		try {
 			/** @var ?Plugin_Update $plugin_information */
-			$plugin_information = $this->api->get_check_update( $should_refresh );
+			$plugin_information = $this->api->get_check_update( $this->force_refresh );
 		} catch ( \BrianHenryIE\WP_SLSWC_Client\Exception\Licence_Does_Not_Exist_Exception $exception ) {
-			$this->logger->debug( 'Licence key not set, not updating plugin information.' );
-			return $value;
+			$this->logger->debug( 'Licence does not exist no server.' );
+			return $afalse;
 		}
 
-		if ( is_null( $plugin_information ) ) {
-			return $value;
-		}
+		return is_null( $plugin_information )
+			? $afalse
+			: $this->convert_to_array( $plugin_information );
+	}
 
-		$plugin =
-			$value->response[ $this->settings->get_plugin_basename() ] ??
-			$value->no_update[ $this->settings->get_plugin_basename() ] ??
-			new stdClass();
-
-		$plugin->id     = $this->settings->get_plugin_basename();
-		$plugin->slug   = $this->settings->get_plugin_slug();
-		$plugin->plugin = $this->settings->get_plugin_basename();
-
-		/**
-		 * If `package` is empty, WordPress will display:
-		 * "Automatic update is unavailable for this plugin."
-		 */
-		$plugin->package     = $plugin_information->get_package();
-		$plugin->new_version = $plugin_information->get_version();
-
-		$plugin->url = $plugin_information->get_url();
-
-		// 'id' => 'w.org/plugins/woocommerce',
-		// 'slug' => 'woocommerce',
-		// 'plugin' => 'woocommerce/woocommerce.php',
-		// 'new_version' => '8.3.1',
-		// 'url' => 'https://wordpress.org/plugins/woocommerce/',
-		// 'package' => 'https://downloads.wordpress.org/plugin/woocommerce.8.3.1.zip',
-		// 'icons' =>
-		// array (
-		// '2x' => 'https://ps.w.org/woocommerce/assets/icon-256x256.gif?rev=2869506',
-		// '1x' => 'https://ps.w.org/woocommerce/assets/icon-128x128.gif?rev=2869506',
-		// ),
-		// 'banners' =>
-		// array (
-		// '2x' => 'https://ps.w.org/woocommerce/assets/banner-1544x500.png?rev=3000842',
-		// '1x' => 'https://ps.w.org/woocommerce/assets/banner-772x250.png?rev=3000842',
-		// ),
-		// 'banners_rtl' =>
-		// array (
-		// ),
-		// 'requires' => '6.3',
-
-		// foreach( get_plugins()[$this->settings->get_plugin_basename()] as $key => $v ) {
-		// if ( ! isset( $plugin->$key ) ) {
-		// $plugin->$key = $v;
-		// }
-		// }
-
-		// TODO: merge with $value.
-		if ( $this->api->is_update_available( false ) ) {
-			$value->response[ $this->settings->get_plugin_basename() ] = $plugin;
-			// unset( $value->no_update[ $this->settings->get_plugin_basename() ] );
-		} else {
-			$value->no_update[ $this->settings->get_plugin_basename() ] = $plugin;
-		}
-
-		return $value;
+	/**
+	 * Convert the Plugin_Update object to an array for use in the `update_plugins` transient.
+	 *
+	 * Not the most elegant solution, but it's the simplest.
+	 *
+	 * @param Plugin_Update $plugin_update
+	 *
+	 * @return Plugin_Update_Array
+	 */
+	protected function convert_to_array( Plugin_Update $plugin_update ): array {
+		return array(
+			'id'           => $plugin_update->get_id(),
+			'slug'         => $plugin_update->get_slug(),
+			'version'      => $plugin_update->get_version(),
+			'url'          => $plugin_update->get_url(),
+			'package'      => $plugin_update->get_package(),
+			'tested'       => $plugin_update->get_tested(),
+			'requires_php' => $plugin_update->get_requires_php(),
+			'autoupdate'   => $plugin_update->get_autoupdate(),
+			'icons'        => $plugin_update->get_icons(),
+			'banners'      => $plugin_update->get_banners(),
+			'banners_rtl'  => $plugin_update->get_banners_rtl(),
+			'translations' => $plugin_update->get_translations(),
+		);
 	}
 }
---
+++ tests/unit/wp-includes/class-wordpress-updater-Unit-Test.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace BrianHenryIE\WP_SLSWC_Client\WP_Includes;
+
+use BrianHenryIE\ColorLogger\ColorLogger;
+use BrianHenryIE\WP_SLSWC_Client\API_Interface;
+use BrianHenryIE\WP_SLSWC_Client\Settings_Interface;
+use Mockery;
+
+/**
+ * @coversDefaultClass \BrianHenryIE\WP_SLSWC_Client\WP_Includes\WordPress_Updater
+ */
+class WordPress_Updater_Unit_Test extends \Codeception\Test\Unit {
+
+	/**
+	 * When the value passed to the set transient function is empty, that implies the transient was deleted
+	 * in order to force an update check. In this case, a synchronous HTTP request is made to the API.
+	 *
+	 * @covers ::detect_force_update
+	 */
+	public function test_force_update(): void {
+
+		$api = Mockery::mock( API_Interface::class )->makePartial();
+
+		$settings = Mockery::mock( Settings_Interface::class )->makePartial();
+		$settings->expects( 'get_plugin_basename' )->andReturn( 'test-plugin/test-plugin.php' );
+		$settings->expects( 'get_plugin_slug' )->andReturn( 'test-plugin' );
+
+		$logger = new ColorLogger();
+
+		$sut = new WordPress_Updater( $api, $settings, $logger );
+
+		$value               = new \stdClass();
+		$value->last_checked = time();
+
+		/**
+		 * @see https://github.com/10up/wp_mock/issues/157
+		 */
+		\WP_Mock::userFunction( 'remove_filter' )
+				->once()
+				->with( 'pre_set_site_transient_update_plugins', array( $sut, 'detect_force_update' ) );
+
+		$sut->detect_force_update( $value, 'update_plugins' );
+
+		$plugin_data = array();
+		$plugin_file = 'test-plugin/test-plugin.php';
+		$locales     = array();
+
+		$api->expects( 'get_check_update' )->once()->with( true );
+
+		$sut->add_update_information( false, $plugin_data, $plugin_file, $locales );
+	}
+}
