0371e65db499c046e62abce7d726bb724cdd8ced
Brian Henry <BrianHenryIE@gmail.com>
2024-06-25 20:41:25 -0700

    Move JS loading to `Admin_Assets` class

--- includes/admin/class-admin-assets.php
+++ includes/admin/class-admin-assets.php
@@ -7,12 +7,60 @@
 
 namespace BrianHenryIE\WP_SLSWC_Client\Admin;
 
+use BrianHenryIE\WP_SLSWC_Client\API_Interface;
+use BrianHenryIE\WP_SLSWC_Client\Licence;
+use BrianHenryIE\WP_SLSWC_Client\Settings_Interface;
+use BrianHenryIE\WP_SLSWC_Client\SLSWC_Client;
+
+use function BrianHenryIE\WP_SLSWC_Client\str_dash_to_next_capitalised_first_lower;
+
 class Admin_Assets {
+
+	public function __construct(
+		protected API_Interface $api,
+		protected Settings_Interface $settings,
+	) {
+	}
 
 	public function register_script(): void {
 	}
 
 	public function enqueue_script(): void {
+
+		$script_handle = "{$this->settings->get_plugin_slug()}-licence";
+
+		$asset_file = include dirname( __DIR__, 2 ) . '/build/index.asset.php';
+
+		wp_enqueue_script(
+			$script_handle,
+			plugins_url( '../../build/index.js', __FILE__ ),
+			$asset_file['dependencies'],
+			$asset_file['version'],
+			true
+		);
+
+		try {
+			$licence_details = $this->api->get_licence_details();
+		} catch ( \Exception $e ) {
+			$licence_details = new Licence();
+		}
+
+		$data = wp_json_encode(
+			array(
+				'restUrl'         => rest_url( "{$this->settings->get_rest_base()}/v1" ),
+				'nonce'           => wp_create_nonce( \BrianHenryIE\WP_SLSWC_Client\WP_Includes\Rest::class ),
+				'licence_details' => $licence_details,
+				// 'licence_details' => $api->get_licence_details(),
+			)
+		);
+
+		$script_var_name = str_dash_to_next_capitalised_first_lower( $script_handle );
+
+		wp_add_inline_script(
+			$script_handle,
+			"const {$script_var_name} = {$data};",
+			'before'
+		);
 	}
 
 	/**
--- includes/functions.php
+++ includes/functions.php
@@ -10,13 +10,36 @@ namespace BrianHenryIE\WP_SLSWC_Client;
 // PRIVATE – API WILL CHANGE WITHOUT NOTICE.
 
 if ( ! function_exists( '\BrianHenryIE\WP_SLSWC_Client\str_underscore_to_dash' ) ) {
+	/**
+	 * Convert a string from snake case to kebab case.
+	 *
+	 * @param string $string
+	 */
 	function str_underscore_to_dash( string $string ): string {
 		return str_replace( '_', '-', $string );
 	}
 }
 
 if ( ! function_exists( '\BrianHenryIE\WP_SLSWC_Client\str_dash_to_underscore' ) ) {
+	/**
+	 * Convert a string from kebab case to snake case.
+	 *
+	 * @param string $string
+	 */
 	function str_dash_to_underscore( string $string ): string {
 		return str_replace( '-', '_', $string );
+	}
+}
+
+if ( ! function_exists( '\BrianHenryIE\WP_SLSWC_Client\str_dash_to_next_capitalised_first_lower' ) ) {
+	/**
+	 * Convert a string from kebab case to camel case.
+	 *
+	 * E.g. `bh-wc-zelle-gateway-licence` -> `bhWcZelleGatewayLicence`.
+	 *
+	 * @param string $string
+	 */
+	function str_dash_to_next_capitalised_first_lower( string $string ): string {
+		return lcfirst( str_replace( ' ', '', ucwords( str_replace( '-', ' ', $string ) ) ) );
 	}
 }
