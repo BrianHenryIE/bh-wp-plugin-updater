75efdd3c4d6e9ae53fa5c46375428aff1566c753
Brian Henry <BrianHenryIE@gmail.com>
2024-06-28 21:04:04 -0700

    Test data – for documentation

---
+++ tests/_data/features/wporg-plugin-update.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * An example response from WordPress.org plugin update check with WooCommerce needing an update.
+ *
+ * @see https://api.wordpress.org/plugins/update-check/1.1/
+ */
+
+return array (
+	'headers' => array(),
+	'body' => '{"plugins":{"woocommerce\\/woocommerce.php":{"id":"w.org\\/plugins\\/woocommerce","slug":"woocommerce","plugin":"woocommerce\\/woocommerce.php","new_version":"9.0.2","url":"https:\\/\\/wordpress.org\\/plugins\\/woocommerce\\/","package":"https:\\/\\/downloads.wordpress.org\\/plugin\\/woocommerce.9.0.2.zip","icons":{"2x":"https:\\/\\/ps.w.org\\/woocommerce\\/assets\\/icon-256x256.gif?rev=2869506","1x":"https:\\/\\/ps.w.org\\/woocommerce\\/assets\\/icon-128x128.gif?rev=2869506"},"banners":{"2x":"https:\\/\\/ps.w.org\\/woocommerce\\/assets\\/banner-1544x500.png?rev=3000842","1x":"https:\\/\\/ps.w.org\\/woocommerce\\/assets\\/banner-772x250.png?rev=3000842"},"banners_rtl":[],"requires":"6.4","tested":"6.5.5","requires_php":"7.4","requires_plugins":[],"compatibility":[]}},"translations":[],"no_update":{"document-generator-for-openapi\\/document-generator-for-openapi.php":{"id":"w.org\\/plugins\\/document-generator-for-openapi","slug":"document-generator-for-openapi","plugin":"document-generator-for-openapi\\/document-generator-for-openapi.php","new_version":"1.1.0","url":"https:\\/\\/wordpress.org\\/plugins\\/document-generator-for-openapi\\/","package":"https:\\/\\/downloads.wordpress.org\\/plugin\\/document-generator-for-openapi.1.1.0.zip","icons":{"default":"https:\\/\\/s.w.org\\/plugins\\/geopattern-icon\\/document-generator-for-openapi.svg"},"banners":[],"banners_rtl":[],"requires":"5.7"},"wp-rest-api-log\\/wp-rest-api-log.php":{"id":"w.org\\/plugins\\/wp-rest-api-log","slug":"wp-rest-api-log","plugin":"wp-rest-api-log\\/wp-rest-api-log.php","new_version":"1.7.0","url":"https:\\/\\/wordpress.org\\/plugins\\/wp-rest-api-log\\/","package":"https:\\/\\/downloads.wordpress.org\\/plugin\\/wp-rest-api-log.1.7.0.zip","icons":{"default":"https:\\/\\/s.w.org\\/plugins\\/geopattern-icon\\/wp-rest-api-log.svg"},"banners":[],"banners_rtl":[],"requires":"4.7"},"wordpress-importer\\/wordpress-importer.php":{"id":"w.org\\/plugins\\/wordpress-importer","slug":"wordpress-importer","plugin":"wordpress-importer\\/wordpress-importer.php","new_version":"0.8.2","url":"https:\\/\\/wordpress.org\\/plugins\\/wordpress-importer\\/","package":"https:\\/\\/downloads.wordpress.org\\/plugin\\/wordpress-importer.0.8.2.zip","icons":{"1x":"https:\\/\\/ps.w.org\\/wordpress-importer\\/assets\\/icon.svg?rev=2791650","svg":"https:\\/\\/ps.w.org\\/wordpress-importer\\/assets\\/icon.svg?rev=2791650"},"banners":{"1x":"https:\\/\\/ps.w.org\\/wordpress-importer\\/assets\\/banner-772x250.png?rev=547654"},"banners_rtl":[],"requires":"5.2"}}}',
+	'response' =>
+		array (
+			'code' => 200,
+			'message' => 'OK',
+		),
+	'cookies' => array(),
+	'filename' => NULL,
+	'http_response' => array(),
+);
---
+++ tests/_data/get-site-transient-update-plugins.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * Sample data from a page-load of `wp-admin/plugins.php`.
+ *
+ * @see wp_update_plugins()
+ *
+ * @package brianhenryie/bh-wp-slswc-client
+ */
+
+return (object) array(
+	'last_checked' => 1718579537,
+	'response' =>
+		array (
+			'woocommerce/woocommerce.php' =>
+				(object) array(
+					'id' => 'w.org/plugins/woocommerce',
+					'slug' => 'woocommerce',
+					'plugin' => 'woocommerce/woocommerce.php',
+					'new_version' => '8.9.3',
+					'url' => 'https://wordpress.org/plugins/woocommerce/',
+					'package' => 'https://downloads.wordpress.org/plugin/woocommerce.8.9.3.zip',
+					'icons' =>
+						array (
+							'2x' => 'https://ps.w.org/woocommerce/assets/icon-256x256.gif?rev=2869506',
+							'1x' => 'https://ps.w.org/woocommerce/assets/icon-128x128.gif?rev=2869506',
+						),
+					'banners' =>
+						array (
+							'2x' => 'https://ps.w.org/woocommerce/assets/banner-1544x500.png?rev=3000842',
+							'1x' => 'https://ps.w.org/woocommerce/assets/banner-772x250.png?rev=3000842',
+						),
+					'banners_rtl' =>
+						array (
+						),
+					'requires' => false,
+					'tested' => '6.5.4',
+					'requires_php' => false,
+					'requires_plugins' =>
+						array (
+						),
+					'autoupdate' => true,
+					'upgrade_notice' => 'Version 8.9.3 contains security fixes and is highly recommended for all users.',
+				),
+			'test-plugin/test-plugin.php' =>
+				(object) array(
+					'id' => 'test-plugin/test-plugin.php',
+					'slug' => 'test-plugin',
+					'plugin' => 'test-plugin/test-plugin.php',
+					'package' => 'https://updatestest.bhwp.ie/?download_file=21&order=wc_order_lckerVdejfHHI&email&key=40bb2001-48c3-4633-995a-447aa82b491d',
+					'new_version' => '1.2.0',
+					'url' => 'https://updatestest.bhwp.ie/product/test-plugin/',
+				),
+		),
+	'translations' =>
+		array (
+		),
+	'no_update' =>
+		array (
+			'document-generator-for-openapi/document-generator-for-openapi.php' =>
+				(object) array(
+					'id' => 'w.org/plugins/document-generator-for-openapi',
+					'slug' => 'document-generator-for-openapi',
+					'plugin' => 'document-generator-for-openapi/document-generator-for-openapi.php',
+					'new_version' => '1.1.0',
+					'url' => 'https://wordpress.org/plugins/document-generator-for-openapi/',
+					'package' => 'https://downloads.wordpress.org/plugin/document-generator-for-openapi.1.1.0.zip',
+					'icons' =>
+						array (
+							'default' => 'https://s.w.org/plugins/geopattern-icon/document-generator-for-openapi.svg',
+						),
+					'banners' =>
+						array (
+						),
+					'banners_rtl' =>
+						array (
+						),
+					'requires' => '5.7',
+				),
+			'hello.php' =>
+				(object) array(
+					'id' => 'w.org/plugins/hello-dolly',
+					'slug' => 'hello-dolly',
+					'plugin' => 'hello.php',
+					'new_version' => '1.7.2',
+					'url' => 'https://wordpress.org/plugins/hello-dolly/',
+					'package' => 'https://downloads.wordpress.org/plugin/hello-dolly.1.7.3.zip',
+					'icons' =>
+						array (
+							'2x' => 'https://ps.w.org/hello-dolly/assets/icon-256x256.jpg?rev=2052855',
+							'1x' => 'https://ps.w.org/hello-dolly/assets/icon-128x128.jpg?rev=2052855',
+						),
+					'banners' =>
+						array (
+							'2x' => 'https://ps.w.org/hello-dolly/assets/banner-1544x500.jpg?rev=2645582',
+							'1x' => 'https://ps.w.org/hello-dolly/assets/banner-772x250.jpg?rev=2052855',
+						),
+					'banners_rtl' =>
+						array (
+						),
+					'requires' => '4.6',
+				),
+			'wp-rest-api-log/wp-rest-api-log.php' =>
+				(object) array(
+					'id' => 'w.org/plugins/wp-rest-api-log',
+					'slug' => 'wp-rest-api-log',
+					'plugin' => 'wp-rest-api-log/wp-rest-api-log.php',
+					'new_version' => '1.7.0',
+					'url' => 'https://wordpress.org/plugins/wp-rest-api-log/',
+					'package' => 'https://downloads.wordpress.org/plugin/wp-rest-api-log.1.7.0.zip',
+					'icons' =>
+						array (
+							'default' => 'https://s.w.org/plugins/geopattern-icon/wp-rest-api-log.svg',
+						),
+					'banners' =>
+						array (
+						),
+					'banners_rtl' =>
+						array (
+						),
+					'requires' => '4.7',
+				),
+			'wordpress-importer/wordpress-importer.php' =>
+				(object) array(
+					'id' => 'w.org/plugins/wordpress-importer',
+					'slug' => 'wordpress-importer',
+					'plugin' => 'wordpress-importer/wordpress-importer.php',
+					'new_version' => '0.8.2',
+					'url' => 'https://wordpress.org/plugins/wordpress-importer/',
+					'package' => 'https://downloads.wordpress.org/plugin/wordpress-importer.0.8.2.zip',
+					'icons' =>
+						array (
+							'1x' => 'https://ps.w.org/wordpress-importer/assets/icon.svg?rev=2791650',
+							'svg' => 'https://ps.w.org/wordpress-importer/assets/icon.svg?rev=2791650',
+						),
+					'banners' =>
+						array (
+							'1x' => 'https://ps.w.org/wordpress-importer/assets/banner-772x250.png?rev=547654',
+						),
+					'banners_rtl' =>
+						array (
+						),
+					'requires' => '5.2',
+				),
+		),
+	'checked' =>
+		array (
+			'document-generator-for-openapi/document-generator-for-openapi.php' => '1.1.0',
+			'hello.php' => '1.7.2',
+			'wp-rest-api-log/wp-rest-api-log.php' => '1.7.0',
+			'slswc/slswc.php' => '1.1.0',
+			'test-plugin/test-plugin.php' => '1.1.1',
+			'woocommerce/woocommerce.php' => '8.9.2',
+			'wordpress-importer/wordpress-importer.php' => '0.8.2',
+		),
+);
