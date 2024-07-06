3162fd94136a5ba7e47ec44c35e88b9109364126
Brian Henry <BrianHenryIE@gmail.com>
2024-07-05 21:45:36 -0700

    Move code from test-plugin to bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,9 @@
 
 namespace BrianHenryIE\WP_Plugin_Updater;
 
+use BrianHenryIE\WP_Logger\Logger;
+use Psr\Log\NullLogger;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	return;
 }
@@ -21,6 +24,32 @@ if ( ! function_exists( '\\BrianHenryIE\\WP_Plugin_Updater\\init_plugin_updater'
 	 * `remove_action( 'plugins_loaded', '\\BrianHenryIE\\WP_Plugin_Updater\\init_plugin_updater' );`
 	 */
 	function init_plugin_updater(): void {
+
+		Plugin_Updater::get_instance(
+			new class() implements Settings_Interface {
+				use Settings_Trait;
+
+				/**
+				 * The plugin basename. Used to add the Logs link on `plugins.php`.
+				 *
+				 * @see https://core.trac.wordpress.org/ticket/42670
+				 *
+				 * @throws \Exception When it cannot be determined. I.e. a symlink inside a symlink.
+				 */
+				public function get_plugin_basename(): string {
+
+					// TODO: The following might work but there are known issues around symlinks that need to be tested and handled correctly.
+					// @see  https://core.trac.wordpress.org/ticket/42670
+
+					$wp_plugin_basename = plugin_basename( __DIR__ );
+
+					$plugin_filename = get_plugins( explode( '/', $wp_plugin_basename )[0] );
+
+					return array_key_first( $plugin_filename );
+				}
+			},
+			new NullLogger()
+		);
 	}
 
 	add_action( 'plugins_loaded', '\\BrianHenryIE\\WP_Plugin_Updater\\init_plugin_updater' );
--- test-plugin.php
+++ test-plugin.php
@@ -1,74 +1,11 @@
 <?php
 /**
- * Plugin Name:   Test Plugin
- * Description:   My test plugin description
+ * Plugin Name:   Example Plugin
+ * Description:   A plugin that does nothing.
  * Version:       1.1.1
  * Author:        BrianHenryIE
  * Author URI:    https://bhwp.ie
- * Update URI:    updatestest.bhwp.ie
+ * Update URI:    updatestest.bhwp.ie/wp-json/slswc/v1
  *
  * @package brianhenryie/bh-wp-plugin-updater
  */
-
-namespace BrianHenryIE\WP_Plugin_Updater;
-
-use BrianHenryIE\WP_Logger\Logger;
-
-// TODO: Does this break the openapi generation?
-if ( ! ( is_admin() || wp_doing_cron() || wp_is_serving_rest_request() || defined( 'WP_CLI' ) ) ) {
-	return;
-}
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-remove_action( 'plugins_loaded', '\\BrianHenryIE\\WP_Plugin_Updater\\init_plugin_updater' );
-
-
-$settings = new class() implements \BrianHenryIE\WP_Plugin_Updater\Settings_Interface {
-	use \BrianHenryIE\WP_Plugin_Updater\Settings_Trait;
-
-	public function get_plugin_basename(): string {
-		return plugin_basename( __FILE__ );
-	}
-};
-
-class Init_Slswc_Client {
-
-	public function __construct(
-		protected Settings_Interface $settings
-	) {
-	}
-
-	/**
-	 * @hooked plugins_loaded
-	 */
-	public function init_slswc(): void {
-
-		$logger_settings = new class() implements \BrianHenryIE\WP_Logger\Logger_Settings_Interface {
-			use \BrianHenryIE\WP_Logger\Logger_Settings_Trait;
-
-			public function get_plugin_basename(): string {
-				return plugin_basename( __FILE__ );
-			}
-
-			public function get_log_level(): string {
-				return 'debug';
-			}
-
-			public function get_cli_base(): ?string {
-				return null;
-			}
-		};
-
-		$logger = Logger::instance( $logger_settings );
-
-		Plugin_Updater::get_instance(
-			$this->settings,
-			$logger
-		);
-	}
-}
-
-$init_slswc = new Init_Slswc_Client( $settings );
-
-add_action( 'plugins_loaded', array( $init_slswc, 'init_slswc' ), 0 );
