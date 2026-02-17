392659eb7a4f71e0fa381d679f27caff0bfb4e85
Brian Henry <BrianHenryIE@gmail.com>
2026-02-16 20:47:51 -0800

    Use named parameters, destruct array for constructor

--- tests/wpunit/class-api-WPUnit-Test.php
+++ tests/wpunit/class-api-WPUnit-Test.php
@@ -6,6 +6,7 @@ use BrianHenryIE\WP_Plugin_Updater\Integrations\Integration_Factory_Interface;
 use BrianHenryIE\WP_Plugin_Updater\Integrations\Integration_Interface;
 use BrianHenryIE\WP_Plugin_Updater\Model\Plugin_Update;
 use Mockery;
+use WP\MCP\Plugin;
 
 /**
  * @coversDefaultClass \BrianHenryIE\WP_Plugin_Updater\API
@@ -57,18 +58,18 @@ class API_WPUnit_Test extends \BrianHenryIE\WP_Plugin_Updater\WPUnit_Testcase {
 	 */
 	public function test_plugin_update_cache_set(): void {
 		$plugin_update = new Plugin_Update(
-			'id',
-			'slug',
-			'version',
-			'url',
-			'package',
-			'tested',
-			'8.0.0',
-			true,
-			array( 'icons' ),
-			array( 'banners' ),
-			array( 'banners_rtl' ),
-			array( 'translations' )
+			id: 'id',
+			slug: 'slug',
+			version: 'version',
+			url: 'url',
+			package: 'package',
+			tested: 'tested',
+			requires_php: '8.0.0',
+			autoupdate: true,
+			icons: array( 'icons' ),
+			banners: array( 'banners' ),
+			banners_rtl: array( 'banners_rtl' ),
+			translations: array( 'translations' ),
 		);
 
 		$settings = Mockery::mock( Settings_Interface::class )->makePartial();
@@ -83,7 +84,8 @@ class API_WPUnit_Test extends \BrianHenryIE\WP_Plugin_Updater\WPUnit_Testcase {
 
 		$result = $sut->get_check_update( true );
 
-		$saved_plugin_update = get_option( 'a_plugin_update' );
+		/** @var Plugin_Update $saved_plugin_update */
+		$saved_plugin_update = new Plugin_Update( ...get_option( 'a_plugin_update' ) );
 
 		$this->assertEquals( $result?->version, $saved_plugin_update->version );
 	}
@@ -94,18 +96,18 @@ class API_WPUnit_Test extends \BrianHenryIE\WP_Plugin_Updater\WPUnit_Testcase {
 	 */
 	public function test_plugin_update_cache_get(): void {
 		$plugin_update = new Plugin_Update(
-			'id',
-			'slug',
-			'version',
-			'url',
-			'package',
-			'tested',
-			'8.0.0',
-			true,
-			array( 'icons' ),
-			array( 'banners' ),
-			array( 'banners_rtl' ),
-			array( 'translations' )
+			id: 'id',
+			slug: 'slug',
+			version: 'version',
+			url: 'url',
+			package: 'package',
+			tested: 'tested',
+			requires_php: '8.0.0',
+			autoupdate: true,
+			icons: array( 'icons' ),
+			banners: array( 'banners' ),
+			banners_rtl: array( 'banners_rtl' ),
+			translations: array( 'translations' ),
 		);
 
 		$settings = Mockery::mock( Settings_Interface::class )->makePartial();
