4272f74bbd7c1ffe4ff1da6114d2043702cd0288
Brian Henry <BrianHenryIE@gmail.com>
2026-02-16 17:02:59 -0800

    Use PHP 8.1 public readonly properties

--- includes/integrations/github/model/class-user.php
+++ includes/integrations/github/model/class-user.php
@@ -10,96 +10,24 @@ namespace BrianHenryIE\WP_Plugin_Updater\Integrations\GitHub\Model;
 class User {
 
 	public function __construct(
-		protected string $login,
-		protected int $id,
-		protected string $node_id,
-		protected string $avatar_url,
-		protected string $gravatar_id,
-		protected string $url,
-		protected string $html_url,
-		protected string $followers_url,
-		protected string $following_url,
-		protected string $gists_url,
-		protected string $starred_url,
-		protected string $subscriptions_url,
-		protected string $organizations_url,
-		protected string $repos_url,
-		protected string $events_url,
-		protected string $received_events_url,
-		protected string $type,
-		protected bool $site_admin
+		public readonly string $login,
+		public readonly int $id,
+		public readonly string $node_id,
+		public readonly string $avatar_url,
+		public readonly string $gravatar_id,
+		public readonly string $url,
+		public readonly string $html_url,
+		public readonly string $followers_url,
+		public readonly string $following_url,
+		public readonly string $gists_url,
+		public readonly string $starred_url,
+		public readonly string $subscriptions_url,
+		public readonly string $organizations_url,
+		public readonly string $repos_url,
+		public readonly string $events_url,
+		public readonly string $received_events_url,
+		public readonly string $type,
+		public readonly bool $site_admin
 	) {
-	}
-
-	public function get_login(): string {
-		return $this->login;
-	}
-
-	public function get_id(): int {
-		return $this->id;
-	}
-
-	public function get_node_id(): string {
-		return $this->node_id;
-	}
-
-	public function get_avatar_url(): string {
-		return $this->avatar_url;
-	}
-
-	public function get_gravatar_id(): string {
-		return $this->gravatar_id;
-	}
-
-	public function get_url(): string {
-		return $this->url;
-	}
-
-	public function get_html_url(): string {
-		return $this->html_url;
-	}
-
-	public function get_followers_url(): string {
-		return $this->followers_url;
-	}
-
-	public function get_following_url(): string {
-		return $this->following_url;
-	}
-
-	public function get_gists_url(): string {
-		return $this->gists_url;
-	}
-
-	public function get_starred_url(): string {
-		return $this->starred_url;
-	}
-
-	public function get_subscriptions_url(): string {
-		return $this->subscriptions_url;
-	}
-
-	public function get_organizations_url(): string {
-		return $this->organizations_url;
-	}
-
-	public function get_repos_url(): string {
-		return $this->repos_url;
-	}
-
-	public function get_events_url(): string {
-		return $this->events_url;
-	}
-
-	public function get_received_events_url(): string {
-		return $this->received_events_url;
-	}
-
-	public function get_type(): string {
-		return $this->type;
-	}
-
-	public function is_site_admin(): bool {
-		return $this->site_admin;
 	}
 }
