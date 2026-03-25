d0267d460ba8cc433161f4bc53da63af0c17bcd6
Brian Henry <BrianHenryIE@gmail.com>
2026-03-25 08:57:55 -0700

    notes

---
+++ development-plugin/ui/class-saved-data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Adds an admin page displaying what data has been saved by the plugin.
+ */
+
+namespace BrianHenryIE\WP_Plugin_Updater\Development_Plugin\UI;
+
+// wp option delete bh_wp_aws_ses_bounce_handler_plugin_information
+
+class Save_Data {
+
+}
